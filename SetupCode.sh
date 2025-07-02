67557e56b9cabab5c230b09c7e585b9cf8b5120e
NghiaNguyen1506 <nghianguyen150006@gmail.com>
2025-07-02 11:16:13 +0700

    SetupCode.sh

--- SetupCode.sh
+++ SetupCode.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Cấp quyền truy cập bộ nhớ cho Termux
 termux-setup-storage
 
-# Cập nhật hệ thống
 pkg update -y
 pkg upgrade -y
 
-# Cài đặt các gói cần thiết
 pkg install -y python
 pkg install -y python-pip
 pkg install -y tsu
 pkg install -y libexpat
 pkg install -y openssl
 
-# Cài đặt thư viện Python
 pip install requests
 pip install psutil
 
-echo "✅ Cài đặt hoàn tất."
+echo "✅ Setup Complete"
